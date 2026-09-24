ade39f9b07b1211745926d28047607d8fbdbb01d
Pipetto-crypto <Pipetto-crypto@pipetto-crypto.com>
2026-09-24 08:37:31 +0000

    renderer_jni: do not display windows whose children are same sized surfaces

--- app/src/main/cpp/winlator/renderer/displayx.cpp
+++ app/src/main/cpp/winlator/renderer/displayx.cpp
@@ -286,6 +286,7 @@ void DisplayX::networkThreadLoop() {
                                 swapchain->images[j] = std::move(drawable);
                             }
                              
+                            if (window->parent) window->parent->surface = window;
                             clientSwapchains[id] = std::move(swapchain);
                             break;
                         }    
@@ -313,6 +314,7 @@ void DisplayX::networkThreadLoop() {
                             
                             auto lock = presentLock.lock();   
                             
+                            swapchain->window->externalContent = drawable;
                             auto presentRequest = std::make_unique<PresentRequest>();
                             presentRequest->drawable = drawable;
                             presentRequest->sync_fence = fence;
@@ -335,7 +337,7 @@ void DisplayX::networkThreadLoop() {
                             if (!swapchain)
                                 continue;
                             
-                            swapchain->window->currentDirectContent = nullptr;
+                            swapchain->window->externalContent = nullptr;
                             clientSwapchains.erase(id);
                             break;
                         }
@@ -660,13 +662,14 @@ void DisplayX::queueEvent(std::function<void()> func) {
 void DisplayX::requestWindowUpdate(Window *window) {
     auto lock = presentLock.lock();
     
+    if (!window || window->isHidden()) return;
     auto drawable = window->drawable.get();
     
     if (effectComposer->isSuitableForColorSwap(drawable))
         effectComposer->apply(drawable);
         
     auto presentRequest = std::make_unique<PresentRequest>();
-    presentRequest->drawable = window->hasDirectContents() ? window->currentDirectContent : drawable;
+    presentRequest->drawable = window->hasExternalContents() ? window->externalContent : drawable;
     presentRequest->sync_fence = -1;
     presentRequest->presentId = -1;
     presentRequest->clientFd = -1;
